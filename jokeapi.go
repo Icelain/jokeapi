a2e61f7aa3904900a4ff3ae3d9413a4bccf7c7fd
Icelain <xerneas965@gmail.com>
2020-11-23 10:36:48 +0530

    Added comments

--- jokeapi.go
+++ jokeapi.go
@@ -9,12 +9,14 @@ import (
 
 var baseURL string="https://sv443.net/jokeapi/v2/joke/"
 
+//Parameters to be accepted by a function
 type Params struct{
 	Categories *[]string
 	Blacklist *[]string
 	JokeType *string
 }
 
+//Response returned to the user
 type JokesResp struct{
 	Error bool
 	Category string
@@ -25,12 +27,13 @@ type JokesResp struct{
 	Lang string
 }
 
-
+//Main struct
 type JokeAPI struct{
 
 	response map[string] interface{}
 
 }
+//Get function. Accepts a Params struct
 func (j *JokeAPI) Get(params Params) JokesResp{
 
 	var mainURL string=""
@@ -57,29 +60,32 @@ func (j *JokeAPI) Get(params Params) JokesResp{
 	}
 	
 //param handling ends here
+	//Sends request
 	resp, err:= http.Get(mainURL)
 	if err!=nil{
 		panic(err)
 	}
-
+	//Converts to readable bytecode
 	info, err:= ioutil.ReadAll(resp.Body)
 	if err!=nil{
 		panic(err)
 	}
-
+	//Unmarshalls json content to map[string] interface
 	json.Unmarshal(info, &j.response)
 
 	jo := []string{}
-
+	
+	//Checks if joke type is single or twopart and saves to string slice accordingly
 	if j.response["type"].(string)=="single"{
 		jo = append(jo, j.response["joke"].(string))
 
 	} else {
 		jo = append(jo, j.response["setup"].(string), j.response["delivery"].(string))
 	}
-
+	//Convert flags response to a map[string]interface type
 	flagInterface := j.response["flags"].(map[string]interface{})
-
+	
+	//Converts flagInterface to map[string] bool
 	flags := map[string]bool{
 		"nsfw": flagInterface["nsfw"].(bool),
 		"religious": flagInterface["religious"].(bool),
@@ -89,7 +95,7 @@ func (j *JokeAPI) Get(params Params) JokesResp{
 	}
 
 	
-
+	//Returns response
 	return JokesResp{
 		Error : j.response["error"].(bool),
 		Category: j.response["category"].(string),
