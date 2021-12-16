4e187b2cb0dab2a2836417efc641860ee6e6aea3
Icelain <xerneas965@gmail.com>
2021-12-16 18:14:27 +0530

    Added tests and shifted from reflection to pure structs

--- jokeapi.go
+++ jokeapi.go
@@ -1,23 +1,35 @@
 package jokeapi
 
 import (
-	"errors"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"strings"
 )
 
 var (
-        baseURL string = "https://v2.jokeapi.dev/"	
+	baseURL string = "https://v2.jokeapi.dev/joke/"
 )
+
+type JokeConsumer struct {
+	Error    bool            `json:"error"`
+	Category string          `json:"category"`
+	Type     string          `json:"type"`
+	ID       float64         `json:"id"`
+	Lang     string          `json:"lang"`
+	Flags    map[string]bool `json:"flags"`
+	Joke     string          `json:"joke"`
+	Setup    string          `json:"setup"`
+	Delivery string          `json:"delivery"`
+}
 
 // Parameters to be used by JokeAPI{}.Fetch()
 type Params struct {
 	Categories []string
 	Blacklist  []string
 	JokeType   string
-	Lang string
+	Lang       string
 }
 
 // Response to be sent by JokeAPI{}.Fetch()
@@ -38,19 +50,20 @@ type JokeAPI struct {
 
 func setSign(sign *string) {
 
-	if *sign=="?" {
+	if *sign == "?" {
 		*sign = "&"
 	}
 }
 
 // Fetches content with respect to the parameters
 func (j *JokeAPI) Fetch() (JokesResp, error) {
-	
+
 	var (
-		response = map[string]interface{}{}
-		mainURL string
-		sign string = "?"
- 	)
+		//response = map[string]interface{}{}
+		jokeConsumer JokeConsumer
+		mainURL      string
+		sign         string = "?"
+	)
 
 	//param handling begins here
 	if len(j.ExportedParams.Categories) > 0 {
@@ -59,24 +72,23 @@ func (j *JokeAPI) Fetch() (JokesResp, error) {
 		mainURL = baseURL + "Any"
 	}
 
-	if len(j.ExportedParams.Blacklist) > 0{
-		
-		mainURL +=  sign + "blacklistFlags=" + strings.Join(j.ExportedParams.Blacklist, ",")
-		
+	if len(j.ExportedParams.Blacklist) > 0 {
+
+		mainURL += sign + "blacklistFlags=" + strings.Join(j.ExportedParams.Blacklist, ",")
+
 		setSign(&sign)
 	}
-	
 
 	if j.ExportedParams.JokeType != "" {
 
-		mainURL +=  sign + "type=" + j.ExportedParams.JokeType
+		mainURL += sign + "type=" + j.ExportedParams.JokeType
 		setSign(&sign)
 	}
-	
+
 	if j.ExportedParams.Lang != "" {
 
 		mainURL += sign + "lang=" + j.ExportedParams.Lang
-		
+
 	}
 
 	//param handling ends here
@@ -87,52 +99,42 @@ func (j *JokeAPI) Fetch() (JokesResp, error) {
 
 	info, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return JokesResp{} ,err	
+		return JokesResp{}, err
 	}
 
-	if err = json.Unmarshal(info, &response); err != nil {
-		
-		return JokesResp{}, errors.New("no joke found for your configuration")
-		
+	if err = json.Unmarshal(info, &jokeConsumer); err != nil {
+
+		return JokesResp{}, errors.New("no joke found for your configuration: ")
+
 	}
 
 	jo := []string{}
-	
-	if response["type"] == nil {
-	
+
+	if jokeConsumer.Type == "" {
+
 		return JokesResp{}, errors.New("no joke found for your configuration")
-		
+
 	}
-	
-	if response["type"].(string) == "single" {
-		jo = append(jo, response["joke"].(string))
+
+	if jokeConsumer.Type == "single" {
+		jo = append(jo, jokeConsumer.Joke)
 
 	} else {
-		jo = append(jo, response["setup"].(string), response["delivery"].(string))
-	}
-
-	flagInterface := response["flags"].(map[string]interface{})
-
-	flags := map[string]bool{
-		"nsfw":      flagInterface["nsfw"].(bool),
-		"religious": flagInterface["religious"].(bool),
-		"racist":    flagInterface["racist"].(bool),
-		"sexist":    flagInterface["sexist"].(bool),
-		"political": flagInterface["political"].(bool),
+		jo = append(jo, jokeConsumer.Setup, jokeConsumer.Delivery)
 	}
 
 	return JokesResp{
-		Error:    response["error"].(bool),
-		Category: response["category"].(string),
-		JokeType: response["type"].(string),
+		Error:    jokeConsumer.Error,
+		Category: jokeConsumer.Category,
+		JokeType: jokeConsumer.Type,
 		Joke:     jo,
-		Flags:    flags,
-		Id:       response["id"].(float64),
-		Lang:     response["lang"].(string),
+		Flags:    jokeConsumer.Flags,
+		Id:       jokeConsumer.ID,
+		Lang:     jokeConsumer.Lang,
 	}, nil
 }
 
-//Sets parameters to JokeAPI struct instance. This method only exists because I don't want to make breaking changes to the existing api by removing it. I would recommend using Jokeapi{}.Set() or the singular methods instead  
+//Sets parameters to JokeAPI struct instance. This method only exists because I don't want to make breaking changes to the existing api by removing it. I would recommend using Jokeapi{}.Set() or the singular methods instead
 func (j *JokeAPI) SetParams(ctgs []string, blacklist []string, joketype string, lang string) {
 
 	j.ExportedParams.Categories = ctgs
@@ -161,6 +163,7 @@ func (j *JokeAPI) SetBlacklist(b []string) {
 	j.ExportedParams.Blacklist = b
 
 }
+
 //Sets language. Go to https://v2.jokeapi.dev/languages?format=txt to select your preferable language format. By default its en (English). Most jokes are available in en and de only and setting other languages will give a corresponding error
 func (j *JokeAPI) SetLang(lang string) {
 
