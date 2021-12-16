70ef38015d587ff191f760c346b51ae6180024ef
Icelain <xerneas965@gmail.com>
2021-12-16 18:25:11 +0530

    added tests

--- jokeapi_test.go
+++ jokeapi_test.go
@@ -15,12 +15,10 @@ func Test_Fetch(t *testing.T) {
 		t.Error(err)
 
 	}
-
 	if len(resp.Joke) == 1 {
 
 		t.Logf("Joke: %s\nID: %.0f", resp.Joke[0], resp.Id)
 		return
 	}
-
 	t.Logf("Setup: %s\nDelivery: %s\n ID: %.0f", resp.Joke[0], resp.Joke[1], resp.Id)
 }
