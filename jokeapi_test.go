bdf44171f1289351e7990a1acf94101172146132
Icelain <xerneas965@gmail.com>
2021-12-21 18:18:49 +0530

    Added new tests

--- jokeapi_test.go
+++ jokeapi_test.go
@@ -4,23 +4,59 @@ import (
 	"testing"
 )
 
-func Test_Fetch(t *testing.T) {
+func Test_Fetch_Parts(t *testing.T) {
 
 	api := New()
-	api.Set(Params{Categories: []string{"Programming"}, Lang: "de"})
 	resp, err := api.Fetch()
 
 	if err != nil {
 
-		t.Error(err)
+		t.Fatal(err)
 
 	}
 
 	if len(resp.Joke) == 1 {
 
-		t.Logf("Joke: %s\nID: %.0f", resp.Joke[0], resp.Id)
-		return
+		if !(resp.JokeType == "single" && resp.Joke[0] != "") {
+
+			t.FailNow()
+
+		}
 	}
 
-	t.Logf("Setup: %s\nDelivery: %s\n ID: %.0f", resp.Joke[0], resp.Joke[1], resp.Id)
+	if len(resp.Joke) == 2 {
+
+		if !(resp.JokeType == "twopart" && resp.Joke[0] != "" && resp.Joke[1] != "") {
+			t.FailNow()
+
+		}
+	}
+}
+
+func Test_Fetch_SetAll(t *testing.T) {
+
+	api := New()
+	api.SetBlacklist([]string{"nsfw"})
+	api.SetLang("de")
+
+	resp, err := api.Fetch()
+
+	if err != nil {
+
+		t.Fatal(err)
+
+	}
+
+	if resp.Flags["nsfw"] {
+
+		t.Error("blacklist flag not set properly")
+
+	}
+
+	if resp.Lang != "de" {
+
+		t.Error("language flag not set properly")
+
+	}
+
 }
